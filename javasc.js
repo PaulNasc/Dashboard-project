2bed366c1645dc5cb2c41fe344cae173438c15cc
BuildTools <unconfigured@null.spigotmc.org>
2023-08-22 22:43:11 -0300

    first commit

---
+++ javasc.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Química Avançada',
+        productNumber: '85743',
+        paymentStatus: 'Devido',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Ana Catarinaaa',
+        productNumber: '97245',
+        paymentStatus: 'Devolvido',
+        status: 'Rejeitado'
+    },
+    {
+        productName: 'Festa do Sumô',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
